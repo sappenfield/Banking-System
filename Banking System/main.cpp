166f0c50d692b316b7dd15d42067bb233448d119
AJ Sappenfield <aj.sappenfield@colorado.edu>
2024-01-17 22:11:06 -0700

    Initial Commit

---
+++ Banking System/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Banking System
+//
+//  Created by AJ Sappenfield on 1/17/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
